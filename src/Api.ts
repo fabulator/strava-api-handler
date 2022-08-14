ab5ca6185f862fcb10875ff0ab0caf945048f42f
Michal Ozogán <michal@ozogan.eu>
2022-08-14 04:24:51 +0000

    Fixed: Interface of token

--- src/Api.ts
+++ src/Api.ts
@@ -38,6 +38,9 @@ interface Athlete {
 interface Token {
     access_token: string;
     athlete: Athlete;
+    expires_at: number;
+    expires_in: number;
+    refresh_token: string;
     token_type: string;
 }
 
